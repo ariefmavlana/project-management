d62748ceab4258217e5e6a40cc978e1e721cdd54
ariefmavlana <ariefmaulana0808@gmail.com>
2025-07-07 10:19:51 +0700

    comm 5, state

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -1,53 +1,27 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface initialStateTypes {
-  isSidebarCollapsed: boolean
-  isDarkMode: boolean
+  isSidebarCollapsed: boolean;
+  isDarkMode: boolean;
 }
 
 const initialState: initialStateTypes = {
   isSidebarCollapsed: false,
   isDarkMode: false,
-}
+};
 
 export const globalSlice = createSlice({
   name: "global",
   initialState,
   reducers: {
     setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
-      state.isSidebarCollapsed = action.payload
+      state.isSidebarCollapsed = action.payload;
     },
     setIsDarkMode: (state, action: PayloadAction<boolean>) => {
-      state.isDarkMode = action.payload
+      state.isDarkMode = action.payload;
     },
   },
-})
+});
 
-export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions
-export default globalSlice.reducer
-
-export interface initialStateTypes {
-  isSidebarCollapsed: boolean
-  isDarkMode: boolean
-}
-
-const initialState: initialStateTypes = {
-  isSidebarCollapsed: false,
-  isDarkMode: false,
-}
-
-export const globalSlice = createSlice({
-  name: "global",
-  initialState,
-  reducers: {
-    setIsSidebarCollapsed: (state, action: PayloadAction<boolean>) => {
-      state.isSidebarCollapsed = action.payload
-    },
-    setIsDarkMode: (state, action: PayloadAction<boolean>) => {
-      state.isDarkMode = action.payload
-    },
-  },
-})
-
-export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions
-export default globalSlice.reducer
+export const { setIsSidebarCollapsed, setIsDarkMode } = globalSlice.actions;
+export default globalSlice.reducer;
